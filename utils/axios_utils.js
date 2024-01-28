b3953c48f0495408ec7b62aa5b123947e37567db
Bryan Navarro <navarro.b1208@gmail.com>
2024-01-28 08:40:07 -0500

    axios client done

--- utils/axios_utils.js
+++ utils/axios_utils.js
@@ -1,21 +1,56 @@
 const axios = require('axios').default;
 
 function getBooks(){
-    return new Promise(async (resolve, reject) => {
+    return new Promise((resolve, reject) => {
         axios.get("localhost:5000/").then((response) => {
             return resolve(response);
-        }).catch(())
+        }).catch((err)=> {
+            return reject(error)
+        })
     })
 }
 
 function getBookByISBN(isbn){
-
+    return new Promise(async (resolve, reject)=> {
+        try {
+            const response = await axios("localhost:5000/isbn/", {
+                params:{
+                    "isbn":isbn
+                }
+            });
+            return resolve(response);
+        } catch (error) {
+            return reject(error);
+        }
+    });
 }
 
 function getBookByAuthor(author){
-    
+    return new Promise(async (resolve, reject)=> {
+        try {
+            const response = await axios("localhost:5000/author/", {
+                params:{
+                    "author":author
+                }
+            });
+            return resolve(response);
+        } catch (error) {
+            return reject(error);
+        }
+    });
 }
 
 function getBookByTitle(title){
-
+    return new Promise(async (resolve, reject)=> {
+        try {
+            const response = await axios("localhost:5000/title/", {
+                params:{
+                    "title":title
+                }
+            });
+            return resolve(response);
+        } catch (error) {
+            return reject(error);
+        }
+    });
 }
